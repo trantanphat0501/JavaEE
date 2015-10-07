102202ea5c335264369c5fb490dc3b7f275a7a72
Trần Tấn Phát <trantanphat0501@users.noreply.github.com>
2015-10-07 13:17:24 +0700

    Update Phat.java
    Phat->PhatTran

--- HelloWorld/src/com/packtpub/wflydevelopment/chapter2/Phat.java
+++ HelloWorld/src/com/packtpub/wflydevelopment/chapter2/Phat.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Phat
  */
-@WebServlet("/Phat")
+@WebServlet("/PhatTran")
 public class Phat extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
